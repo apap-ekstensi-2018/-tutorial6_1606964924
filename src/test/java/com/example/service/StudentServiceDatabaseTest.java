602a054c1689507d6049a92d5b617c53ed5c4806
Novalina NVH. Hutabarat <novalina.hutabarat@moonlay.jakarta>
2018-03-24 20:56:06 +0700

    Commit tutorial 6

---
+++ src/test/java/com/example/service/StudentServiceDatabaseTest.java
@@ -0,0 +1,116 @@
+package com.example.service;
+import com.example.dao.StudentMapper;
+import com.example.model.StudentModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.*;
+
+public class StudentServiceDatabaseTest {
+	private StudentService studentService = new StudentServiceDatabase();
+	
+	@Mock
+	private StudentMapper studentMapper;
+	
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		this.studentService = new StudentServiceDatabase(this.studentMapper);
+	}
+	
+	@Test
+	public void selectStudent() {
+		//given
+		StudentModel studentModel = new StudentModel("1506737823", "Chanek", 3.5);
+		StudentModel check = new StudentModel("1506737823", "Chanek", 3.5);
+		BDDMockito.given(studentMapper.selectStudent("1506737823")).willReturn(studentModel);
+		
+		//when
+		StudentModel test = studentService.selectStudent("1506737823");
+		
+		//then
+		assertThat(test, notNullValue());
+		assertThat(test, equalTo(check));
+	}
+	
+	@Test
+	public void selectAllStudent() {
+		//Given
+		List<StudentModel> studentModels = new ArrayList<>();
+		StudentModel studentModel = new StudentModel("1506737823", "Chanek", 3.5);
+		studentModels.add(studentModel);
+		
+		List<StudentModel> checks = new ArrayList<>();
+		StudentModel check = new StudentModel("1506737823", "Chanek", 3.5);
+		checks.add(check);
+		
+		BDDMockito.given(studentMapper.selectAllStudents()).willReturn(studentModels);
+		
+		//when
+		List<StudentModel> test = studentService.selectAllStudents();
+		
+		//then
+		assertThat(test, notNullValue());
+		assertThat(test.isEmpty(), equalTo(false));
+		assertThat(test.size(), equalTo(1));
+		assertThat(test, equalTo(checks));
+	}
+	
+	@Test
+	public void addStudent() {
+		//Given
+		StudentModel studentModel = new StudentModel("1506737823", "Chanek", 3.5);
+		StudentModel check = new StudentModel("1506737823", "Chanek", 3.5);
+		
+		BDDMockito.given(studentService.addStudent(studentModel)).willReturn(true);
+		
+		//When 
+		boolean test = studentService.addStudent(studentModel);
+		
+		//Then
+		BDDMockito.then(studentMapper).should().addStudent(check);
+		assertThat(test, equalTo(true));
+	}
+	
+	@Test
+	public void deleteStudent() {
+		//Given
+		StudentModel studentModel = new StudentModel("1506737823", "Chanek", 3.5);
+		BDDMockito.given(studentMapper.deleteStudent("1506737823")).willReturn(true);
+		
+		//When 
+		boolean test = studentService.deleteStudent("1506737823");
+		
+		//Then 
+		BDDMockito.then(studentMapper).should().deleteStudent("1506737823");
+		assertThat(test, equalTo(true));
+	}
+	
+	@Test
+	public void updateStudent() {
+		//Given
+		StudentModel studentModel = new StudentModel("1506737823", "Chanek", 3.5);
+		BDDMockito.given(studentMapper.updateStudent(studentModel)).willReturn(true);
+		
+		//When
+		boolean test = studentService.updateStudent(studentModel);
+		
+		//Then
+		BDDMockito.then(studentMapper).should().updateStudent(studentModel);
+		assertThat(test, equalTo(true));
+		
+	}
+	
+}
